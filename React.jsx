38428af8c9b8a78d0b71d7fa6938d5280a2e4b2c
PhaniKumar199 <srirama99.a@gmail.com>
2024-12-04 21:24:36 +0530

    Just Now changed

--- React.jsx
+++ React.jsx
@@ -6,6 +6,7 @@ const React = () => {
         <h1>Hello</h1>
         <h1>This is Phani Kumar</h1>
         <p>this is changed by mani</p>
+        <h2>Now adding new line by phani</h2>
 
     </div>
   )
